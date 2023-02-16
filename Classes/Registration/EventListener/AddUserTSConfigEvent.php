e9e00f78e2b05da5a5e55eb1a876ed1d8dd05684
Raphael Thanner <r.thanner@zeroseven.de>
2023-02-16 11:01:21 +0100

    Add custom doktypes to the pagetree

---
+++ Classes/Registration/EventListener/AddUserTSConfigEvent.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zeroseven\Rampage\Registration\EventListener;
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use Zeroseven\Rampage\Domain\Model\PageTypeInterface;
+use Zeroseven\Rampage\Registration\Event\StoreRegistrationEvent;
+use Zeroseven\Rampage\Registration\Registration;
+
+class AddUserTSConfigEvent
+{
+    protected ?Registration $registration;
+
+    protected function addPageType(int $documentType): void
+    {
+        ExtensionManagementUtility::addUserTSConfig("options.pageTree.doktypesToShowInNewPageDragArea := addToList($documentType)");
+    }
+
+    protected function addPageTypes(): void
+    {
+
+        if (($pageObject = $this->registration->getObject()) && $pageObject->isEnabled() && is_subclass_of(($className = $pageObject->getObjectClassName()), PageTypeInterface::class)) {
+            $this->addPageType($className::getType());
+        }
+
+        if (($categoryPage = $this->registration->getCategory()) && $categoryPage->isEnabled() && is_subclass_of(($className = $categoryPage->getObjectClassName()), PageTypeInterface::class)) {
+            $this->addPageType($className::getType());
+        }
+    }
+
+    public function __invoke(StoreRegistrationEvent $event): void
+    {
+        $this->registration = $event->getRegistration();
+        $this->addPageTypes();
+    }
+}
