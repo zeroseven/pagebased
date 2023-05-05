424b6970a04494a2b8169c85bd5e9cbc82677a99
Raphael Thanner <r.thanner@zeroseven.de>
2023-05-05 23:25:25 +0200

    Fix datahandler hook

--- Classes/Hooks/DataHandler/IdentifierDetection.php
+++ Classes/Hooks/DataHandler/IdentifierDetection.php
@@ -6,6 +6,7 @@ namespace Zeroseven\Rampage\Hooks\DataHandler;
 
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use Zeroseven\Rampage\Domain\Model\AbstractPage;
 use Zeroseven\Rampage\Utility\IdentifierUtility;
 use Zeroseven\Rampage\Utility\RootLineUtility;
 
@@ -13,12 +14,14 @@ class IdentifierDetection
 {
     protected function updateIdentifier(string $table, mixed $id, array &$fieldArray): void
     {
-        $detector = GeneralUtility::makeInstance(IdentifierUtility::class, $id, $table);
-        $categoryRegistration = $detector->getCategoryRegistration();
-        $objectRegistration = $detector->getObjectRegistration();
+        if ($table === AbstractPage::TABLE_NAME) {
+            $identifierUtility = GeneralUtility::makeInstance(IdentifierUtility::class, $id, $table);
+            $categoryRegistration = $identifierUtility->getCategoryRegistration();
+            $objectRegistration = $identifierUtility->getObjectRegistration();
 
-        $fieldArray[IdentifierUtility::SITE_FIELD_NAME] = ($categoryRegistration || $objectRegistration) ? RootLineUtility::getRootPage((int)$id) : 0;
-        $fieldArray[IdentifierUtility::OBJECT_FIELD_NAME] = $objectRegistration ? $objectRegistration->getClassName() : '';
+            $fieldArray[IdentifierUtility::SITE_FIELD_NAME] = ($categoryRegistration || $objectRegistration) ? RootLineUtility::getRootPage((int)$id) : 0;
+            $fieldArray[IdentifierUtility::OBJECT_FIELD_NAME] = $objectRegistration ? $objectRegistration->getClassName() : '';
+        }
     }
 
     public function processDatamap_postProcessFieldArray(string $status, string $table, mixed $id, array &$fieldArray, DataHandler $dataHandler): void
