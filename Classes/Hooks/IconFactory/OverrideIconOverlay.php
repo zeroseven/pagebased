dc152fcc4f45367f7274a79c67242b826b5c73a7
Raphael Thanner <r.thanner@zeroseven.de>
2023-05-19 11:22:14 +0200

    Update icon hook

--- Classes/Hooks/IconFactory/OverrideIconOverlay.php
+++ Classes/Hooks/IconFactory/OverrideIconOverlay.php
@@ -4,31 +4,26 @@ declare(strict_types=1);
 
 namespace Zeroseven\Rampage\Hooks\IconFactory;
 
-use TYPO3\CMS\Backend\Utility\BackendUtility;
 use TYPO3\CMS\Core\Imaging\IconFactory;
 use Zeroseven\Rampage\Domain\Model\AbstractPage;
-use Zeroseven\Rampage\Registration\RegistrationService;
-use Zeroseven\Rampage\Utility\DetectionUtility;
+use Zeroseven\Rampage\Utility\ObjectUtility;
 
 class OverrideIconOverlay
 {
     public function postOverlayPriorityLookup(string $table, array $row, array $status, string $iconName = null): ?string
     {
-        if ($table === AbstractPage::TABLE_NAME && empty($iconName)) {
-            $uid = $row['uid'] ?? 0;
-            $typeField = $GLOBALS['TCA'][AbstractPage::TABLE_NAME]['ctrl']['type'];
+        if ($table === AbstractPage::TABLE_NAME && empty($iconName) && $uid = (int)($row['uid'] ?? 0)) {
 
-            if (!isset($row[$typeField], $row[DetectionUtility::REGISTRATION_FIELD_NAME])) {
-                $row = (array)BackendUtility::getRecord(AbstractPage::TABLE_NAME, $uid, implode(',', [$typeField, DetectionUtility::REGISTRATION_FIELD_NAME]));
-            }
+            if (($registration = ObjectUtility::isObject($uid)) && $object = $registration->getObject()->getRepositoryClass()->findByUid($uid)) {
+                if ($object->isTop()) {
+                    return 'overlay-approved';
+                }
 
-            if (
-                ($identifier = $row[DetectionUtility::REGISTRATION_FIELD_NAME] ?? null)
-                && ($registration = RegistrationService::getRegistrationByIdentifier($identifier))
-                && ($object = $registration->getObject()->getRepositoryClass()->findByUid($uid))
-                && $object->isTop()
-            ) {
-                return 'overlay-approved';
+                if ($object->getParentObject()) {
+                    return 'overlay-advanced';
+                }
+
+                return 'overlay-list';
             }
         }
 
