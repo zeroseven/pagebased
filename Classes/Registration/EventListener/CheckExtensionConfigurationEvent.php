f3c152f9aed90902913d67babef1173ea548011c
Raphael Thanner <r.thanner@zeroseven.de>
2023-05-19 10:03:18 +0200

    Create extension configuration template if not exists

--- Classes/Registration/EventListener/CheckExtensionConfigurationEvent.php
+++ Classes/Registration/EventListener/CheckExtensionConfigurationEvent.php
@@ -28,6 +28,49 @@ class CheckExtensionConfigurationEvent
         $this->logOnConsole(sprintf('Override property "%s" in "%s" for extension "%s".', $property, (new ReflectionClass($registrationProperty))->getShortName(), $this->registration->getExtensionName()));
     }
 
+    protected function createExtensionConfigurationTemplate(): void
+    {
+        $path = sprintf('%s/%s/ext_conf_template.txt', Environment::getExtensionsPath(), $this->registration->getExtensionName());
+
+        if (!@file_exists($path)) {
+            GeneralUtility::writeFile($path, trim('
+# auto generated file. (' . self::class . ')
+registration {
+    object {
+        # cat=object/enable/10; type=options[default=,enable=1,disable=0]; label=Tags
+        tags =
+        # cat=object/enable/20; type=options[default=,enable=1,disable=0]; label=Top
+        top =
+        # cat=object/enable/30; type=string; label=Comma separated list of topic storage page ids
+        topicPageIds =
+        # cat=object/enable/40; type=string; label=Comma separated list of topic storage page ids
+        contactPageIds =
+        # cat=object/enable/50; type=string; label=Object title
+        title =
+    }
+    category {
+        # cat=category/enable/10; type=string; label=Category title
+        title =
+        # cat=category/enable/20; type=string; label=Icon identifier
+        iconIdentifier =
+    }
+    listPlugin {
+        # cat=listPlugin/enable/10; type=string; label=List plugin title
+        title =
+        # cat=listPlugin/enable/20; type=string; label=Icon identifier
+        iconIdentifier =
+    }
+    filterPlugin {
+        # cat=filterPlugin/enable/10; type=string; label=Filter plugin title
+        title =
+        # cat=filterPlugin/enable/20; type=string; label=Icon identifier
+        iconIdentifier =
+    }
+}
+            '));
+        }
+    }
+
     protected function overrideProperties(RegistrationPropertyInterface $registrationProperty, array $configuration): void
     {
         foreach ($configuration ?? [] as $key => $value) {
@@ -61,6 +104,7 @@ class CheckExtensionConfigurationEvent
     public function __invoke(BeforeStoreRegistrationEvent $event)
     {
         $this->registration = $event->getRegistration();
+        $this->createExtensionConfigurationTemplate();
 
         $overrides = [
             [$this->registration->getObject(), SettingsUtility::getExtensionConfiguration($this->registration, 'registration.object')],
