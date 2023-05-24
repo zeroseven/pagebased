3e4206c5a37e20838d4f41be7a65a54159595b3d
Raphael Thanner <r.thanner@zeroseven.de>
2023-05-24 22:52:31 +0200

    Create detection command

---
+++ Classes/Command/DetectionCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zeroseven\Rampage\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use TYPO3\CMS\Core\Utility\MathUtility;
+use Zeroseven\Rampage\Utility\DetectionUtility;
+use Zeroseven\Rampage\Utility\RootLineUtility;
+
+class DetectionCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this->addArgument('startingPoint', InputArgument::REQUIRED, 'Define the starting point in TYPO3\'s page tree.');
+        $this->addArgument('depth', InputArgument::OPTIONAL, 'Specify how deep to search from the starting point.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $startingPoint = (int)$input->getArgument('startingPoint');
+        $depth = MathUtility::canBeInterpretedAsInteger($value = $input->getArgument('depth')) ? (int)$value : null;
+        $rootLine = RootLineUtility::collectPagesBelow($startingPoint, false, $depth);
+
+        foreach ($rootLine as $uid => $page) {
+            DetectionUtility::updateFields($uid);
+        }
+
+        return 0;
+    }
+}
