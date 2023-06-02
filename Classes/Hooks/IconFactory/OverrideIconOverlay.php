d6f455d85e8ce5c453dd7b1460e79cdf6295a6d0
Raphael Thanner <r.thanner@zeroseven.de>
2023-06-02 13:36:44 +0200

    Add overlay icon on categories

--- Classes/Hooks/IconFactory/OverrideIconOverlay.php
+++ Classes/Hooks/IconFactory/OverrideIconOverlay.php
@@ -13,7 +13,6 @@ class OverrideIconOverlay
     public function postOverlayPriorityLookup(string $table, array $row, array $status, string $iconName = null): ?string
     {
         if ($table === AbstractPage::TABLE_NAME && empty($iconName) && $uid = (int)($row['uid'] ?? 0)) {
-
             if (($registration = ObjectUtility::isObject($uid)) && $object = $registration->getObject()->getRepositoryClass()->findByUid($uid)) {
                 if ($object->isTop()) {
                     return 'overlay-approved';
@@ -24,6 +23,12 @@ class OverrideIconOverlay
                 }
 
                 return 'overlay-list';
+            }
+
+            if (($registration = ObjectUtility::isCategory($uid)) && $category = $registration->getCategory()->getRepositoryClass()->findByUid($uid)) {
+                if ($category->getRedirectCategory()) {
+                    return 'overlay-shortcut';
+                }
             }
         }
 
