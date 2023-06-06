c76116ebe4511f9912a34a2a73553f3869f0e7ea
Raphael Thanner <r.thanner@zeroseven.de>
2023-06-06 21:09:44 +0200

    Add data processor to pass an object to a fluid template

---
+++ Classes/DataProcessing/ObjectProcessor.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zeroseven\Rampage\DataProcessing;
+
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+use Zeroseven\Rampage\Exception\ValueException;
+use Zeroseven\Rampage\Registration\RegistrationService;
+
+class ObjectProcessor implements DataProcessorInterface
+{
+    /** @throws ValueException */
+    public function process(ContentObjectRenderer $cObj, array $contentObjectConfiguration, array $processorConfiguration, array $processedData): array
+    {
+        $registrationIdentifier = $cObj->stdWrapValue('registration', $processorConfiguration, '');
+        $registration = RegistrationService::getRegistrationByIdentifier($registrationIdentifier);
+
+        if ($registration === null) {
+            $validIdentifier = array_map(static fn($registration) => '"' . $registration->getIdentifier() . '"', RegistrationService::getRegistrations());
+
+            throw new ValueException(sprintf('Registration not found, or empty "registration" configuration in %s. Use one of the following identifier %s', self::class, implode(', ', $validIdentifier)), 1623157889);
+        }
+
+        $uid = $cObj->stdWrapValue('id', $processorConfiguration, null) ??
+            (($GLOBALS['TSFE'] ?? null) instanceof TypoScriptFrontendController ? $GLOBALS['TSFE']->id : null);
+
+        if ($uid && $object = $registration->getObject()->getRepositoryClass()->findByUid((int)$uid, true)) {
+            if ($key = $cObj->stdWrapValue('as', $processorConfiguration, null)) {
+                $processedData[$key] = $object;
+            } else {
+                $processedData['object'] = $object;
+                $processedData[strtolower($registration->getObject()->getName())] = $object; // Alias
+            }
+        }
+
+        return $processedData;
+    }
+}
