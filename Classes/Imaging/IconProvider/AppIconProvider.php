2c8608077459a59140e03bf44972a80dbc72da71
Raphael Thanner <r.thanner@zeroseven.de>
2023-06-12 11:47:59 +0200

    Show first letter of category title in icons

--- Classes/Imaging/IconProvider/AppIconProvider.php
+++ Classes/Imaging/IconProvider/AppIconProvider.php
@@ -12,7 +12,7 @@ class AppIconProvider extends AbstractIconProvider
     protected function createImage(Icon $icon, array $options, Registration $registration): string
     {
         return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><g' . ($options['hideInMenu'] ?? false ? ' opacity=".5"' : '') . '><path fill="#EFEFEF" d="M2 0v16h12V4l-4-4H2z"/><path fill="#FFF" d="M10 4V0l4 4h-4z" opacity=".65"/><path fill="#212121" d="M13 5v5L9 5h4z" opacity=".2"/><path fill="#999" d="M2 0v16h12V4l-4-4H2zm1 1h6v4h4v10H3V1zm7 .4L12.6 4H10V1.4z"/><text x="8" y="12" style="fill:#222;font:normal bold 8px arial;text-anchor:middle;">'
-            . strtoupper(substr($registration->getObject()->getName(), 0, 1))
+            . strtoupper(substr($options['letter'] ?? $registration->getCategory()->getTitle(), 0, 1))
             . '</text></g></svg>';
     }
 }
--- Classes/Imaging/IconProvider/OverlayIconProvider.php
+++ Classes/Imaging/IconProvider/OverlayIconProvider.php
@@ -12,7 +12,7 @@ class OverlayIconProvider extends AbstractIconProvider
     protected function createImage(Icon $icon, array $options, Registration $registration): string
     {
         return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11 11"><path fill="#666" d="M1 1h10v10H1z"/><path fill="#FFF" d="M2 2h8v8H2z"/><text x="6" y="8.5" style="fill:#222;font:normal bold 7px arial;text-anchor:middle;">'
-            . strtoupper(substr($registration->getObject()->getName(), 0, 1))
+            . strtoupper(substr($options['letter'] ?? $registration->getCategory()->getTitle(), 0, 1))
             . '</text></svg>';
     }
 }
