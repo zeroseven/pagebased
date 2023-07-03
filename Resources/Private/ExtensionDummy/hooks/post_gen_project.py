e4695e2c553db83e7bec40e024f4f733c0bb6ee2
Raphael Thanner <r.thanner@zeroseven.de>
2023-07-03 13:38:49 +0200

    Add success info after completion of the cookiecutter template

---
+++ Resources/Private/ExtensionDummy/hooks/post_gen_project.py
@@ -0,0 +1,2 @@
+print('🎉 Congratulations! Your new TYPO3 extension "{{ cookiecutter.extension_key }}" has been created.')
+print('👉️ Run "composer req {{ cookiecutter.vendor_name|lower }}/{{ cookiecutter.extension_key.replace('_', '-') }}" to install the extension.')
