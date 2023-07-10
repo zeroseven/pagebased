b5831a9c2db261ef4e0cc97c4de1bc326f27aaef
Raphael Thanner <r.thanner@zeroseven.de>
2023-07-10 11:35:58 +0200

    Support uid's without table prefix in group filter userFunc

--- Classes/Backend/TCA/GroupFilter.php
+++ Classes/Backend/TCA/GroupFilter.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Zeroseven\Rampage\Backend\TCA;
 
 use TYPO3\CMS\Core\DataHandling\DataHandler;
-use Zeroseven\Rampage\Domain\Model\AbstractPage;
+use TYPO3\CMS\Core\Utility\MathUtility;
 use Zeroseven\Rampage\Utility\ObjectUtility;
 
 class GroupFilter
@@ -18,15 +18,20 @@ class GroupFilter
         if ($parent instanceof DataHandler) {
             $uid = (int)array_key_first($parent->datamap[$table] ?? []);
             $registration = ObjectUtility::isObject($uid);
-
             if ($registration && $values) {
                 $newValues = [];
 
                 foreach ($values as $value) {
-                    preg_match('/^(?:([a-z_]+)_)?(\d+)$/', $value, $matches)
-                    && ($recordUid = (int)($matches[2] ?? 0)) && $recordUid !== $uid
-                    && ($matches[1] === AbstractPage::TABLE_NAME)
-                    && ($recordRegistration = ObjectUtility::isObject($recordUid))
+                    if (MathUtility::canBeInterpretedAsInteger($value)) {
+                        $recordId = (int)$value;
+                    } else {
+                        preg_match('/^(?:([a-z_]+)_)?(\d+)$/', $value, $matches);
+                        $recordId = (int)($matches[2] ?? 0);
+                    }
+
+                    $recordId > 0
+                    && $recordId !== $uid
+                    && ($recordRegistration = ObjectUtility::isObject($recordId))
                     && ($recordRegistration->getIdentifier() === $registration->getIdentifier())
                     && ($newValues[] = $value);
                 }
