0042f1632ce1eb642dff754900b2540091823344
Raphael Thanner <r.thanner@zeroseven.de>
2023-08-22 16:28:20 +0200

    Update error handling in rss feed

--- Classes/Middleware/RssFeed.php
+++ Classes/Middleware/RssFeed.php
@@ -12,6 +12,7 @@ use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use TYPO3\CMS\Core\Cache\Frontend\FrontendInterface;
 use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Error\Http\PageNotFoundException;
 use TYPO3\CMS\Core\EventDispatcher\EventDispatcher;
 use TYPO3\CMS\Core\Http\HtmlResponse;
 use TYPO3\CMS\Core\Http\RedirectResponse;
@@ -21,6 +22,8 @@ use TYPO3\CMS\Core\Service\FlexFormService;
 use TYPO3\CMS\Core\Site\Entity\SiteLanguage;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
+use TYPO3\CMS\Frontend\Controller\ErrorController;
+use TYPO3\CMS\Frontend\Page\PageAccessFailureReasons;
 use Zeroseven\Pagebased\Event\Rss\RssFeedEvent;
 use Zeroseven\Pagebased\Registration\Registration;
 use Zeroseven\Pagebased\Registration\RegistrationService;
@@ -98,60 +101,64 @@ class RssFeed implements MiddlewareInterface
             ]
         ];
     }
+
+    /** @throws PageNotFoundException */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        if (
-            str_ends_with($request->getUri()->getPath(), self::URL_SUFFIX)
-            && ($routing = $request->getAttribute('routing')) instanceof RouteResultInterface
-            && ($language = $routing->getLanguage()) instanceof SiteLanguage
-            && ($pid = $this->getPid($request, $routing))
-        ) {
-            $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable(self::TABLE_NAME);
+        if (str_ends_with($request->getUri()->getPath(), self::URL_SUFFIX)) {
+            if (
+                ($routing = $request->getAttribute('routing')) instanceof RouteResultInterface
+                && ($language = $routing->getLanguage()) instanceof SiteLanguage
+                && ($pid = $this->getPid($request, $routing))
+            ) {
+                $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable(self::TABLE_NAME);
 
-            $CTypes = array_filter(array_map(static function (Registration $registration) use ($queryBuilder) {
-                return $registration->hasListPlugin() ? $queryBuilder->quote($registration->getListPlugin()->getCType($registration)) : null;
-            }, RegistrationService::getRegistrations() ?? []));
+                $CTypes = array_filter(array_map(static function (Registration $registration) use ($queryBuilder) {
+                    return $registration->hasListPlugin() ? $queryBuilder->quote($registration->getListPlugin()->getCType($registration)) : null;
+                }, RegistrationService::getRegistrations() ?? []));
 
-            try {
-                $content = empty($CTypes) ? null : $queryBuilder->select('*')
-                    ->from(self::TABLE_NAME)
-                    ->where(
-                        $queryBuilder->expr()->in($GLOBALS['TCA'][self::TABLE_NAME]['ctrl']['type'], $CTypes),
-                        $queryBuilder->expr()->in($GLOBALS['TCA'][self::TABLE_NAME]['ctrl']['languageField'], [-1, $language->getLanguageId()]),
-                        $queryBuilder->expr()->eq('pid', $pid),
-                    )
-                    ->orderBy($GLOBALS['TCA'][self::TABLE_NAME]['ctrl']['sortby'])
-                    ->setMaxResults(1)
-                    ->execute()
-                    ->fetchAllAssociative()[0] ?? null;
+                try {
+                    $content = empty($CTypes) ? null : $queryBuilder->select('*')
+                        ->from(self::TABLE_NAME)
+                        ->where(
+                            $queryBuilder->expr()->in($GLOBALS['TCA'][self::TABLE_NAME]['ctrl']['type'], $CTypes),
+                            $queryBuilder->expr()->in($GLOBALS['TCA'][self::TABLE_NAME]['ctrl']['languageField'], [-1, $language->getLanguageId()]),
+                            $queryBuilder->expr()->eq('pid', $pid),
+                        )
+                        ->orderBy($GLOBALS['TCA'][self::TABLE_NAME]['ctrl']['sortby'])
+                        ->setMaxResults(1)
+                        ->execute()
+                        ->fetchAllAssociative()[0] ?? null;
 
-                if (
-                    $content
-                    && ($CType = $content[$GLOBALS['TCA'][self::TABLE_NAME]['ctrl']['type'] ?? ''])
-                    && ($registration = $this->getRegistrationByCType($CType))
-                    && ($settings = $this->getPluginSettings($content))
-                    && ($objects = $this->getObjects($registration, $settings, $language))
-                ) {
-                    $identifier = md5($registration->getIdentifier() . ($content['uid'] ?? ''));
+                    if (
+                        $content
+                        && ($CType = $content[$GLOBALS['TCA'][self::TABLE_NAME]['ctrl']['type'] ?? ''])
+                        && ($registration = $this->getRegistrationByCType($CType))
+                    ) {
+                        $identifier = md5($registration->getIdentifier() . ($content['uid'] ?? ''));
 
-                    if (empty($rssFeed = $this->cache->get($identifier))) {
-                        $rssFeed = GeneralUtility::makeInstance(EventDispatcher::class)->dispatch(new RssFeedEvent($registration, $request, $settings, $content, $objects))->render();
+                        if (empty($rssFeed = $this->cache->get($identifier))) {
+                            $settings = $this->getPluginSettings($content);
+                            $objects = $this->getObjects($registration, $settings, $language);
+                            $rssFeed = GeneralUtility::makeInstance(EventDispatcher::class)->dispatch(new RssFeedEvent($registration, $request, $settings, $content, $objects))->render();
 
-                        $this->cache->set($identifier, $rssFeed);
+                            $this->cache->set($identifier, $rssFeed);
+                        }
+
+                        return GeneralUtility::makeInstance(HtmlResponse::class, trim('<?xml version="1.1" encoding="utf-8"?>' . $rssFeed), 200, [
+                            'Content-Type' => 'application/rss+xml; charset=utf-8',
+                            'X-Robots-Tag' => 'noindex',
+                            'X-Typo3-Extension' => 'pagebased',
+                            'X-Xml-Identifier' => $identifier
+                        ]);
                     }
-
-                    return GeneralUtility::makeInstance(HtmlResponse::class, trim('<?xml version="1.1" encoding="utf-8"?>' . $rssFeed), 200, [
-                        'Content-Type' => 'application/rss+xml; charset=utf-8',
-                        'X-Robots-Tag' => 'noindex',
-                        'X-Typo3-Extension' => 'pagebased',
-                        'X-Xml-Identifier' => $identifier,
-                        'X-Xml-Items' => $objects->count()
-                    ]);
+                } catch (DBALException | Exception $e) {
                 }
-            } catch (DBALException|Exception $e) {
             }
 
-            return GeneralUtility::makeInstance(RedirectResponse::class, str_replace(self::URL_SUFFIX, '/', $request->getUri()->getPath()), 303);
+            $error = GeneralUtility::makeInstance(PageAccessFailureReasons::class)->getMessageForReason(PageAccessFailureReasons::PAGE_NOT_FOUND);
+
+            return GeneralUtility::makeInstance(ErrorController::class)->pageNotFoundAction($request, $error, ['code' => 404]);
         }
 
         return $handler->handle($request);
