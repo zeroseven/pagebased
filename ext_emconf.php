7c967a34b0181adfe7b795b8688af9a5822b1557
Gratian Alinei <g.alinei@users.zeroseven.de>
2024-09-10 00:41:23 +0300

    62883 [pagebased] Update version and constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'zeroseven design studios GmbH',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
-    'version' => '1.3.1',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99'
+            'typo3' => '13.1.0-13.4.99'
         ],
         'suggests' => [
             'pagebased_blog' => ''
