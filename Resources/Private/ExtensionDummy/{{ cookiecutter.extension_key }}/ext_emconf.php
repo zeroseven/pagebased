c26757092b2666015ec94193cf353a93144b8882
Sebastian Rosskopf <s.rosskopf@zeroseven.de>
2026-04-20 08:33:22 +0200

    [TASK] Update TYPO3 core version requirements to support v13

--- Resources/Private/ExtensionDummy/{{ cookiecutter.extension_key }}/ext_emconf.php
+++ Resources/Private/ExtensionDummy/{{ cookiecutter.extension_key }}/ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
 //  'author_company' => 'Company name',
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
-    'version' => '0.0.0',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '13.4.0-13.4.99',
             'pagebased' => ''
         ]
     ]
